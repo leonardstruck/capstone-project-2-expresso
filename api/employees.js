1dc711e16452ba106a760e16a5d28121f8341d7d
Leonard Struck <info@leonardstruck.de>
2018-08-25 17:04:04 +0200

    GET /api/employees

--- api/employees.js
+++ api/employees.js
@@ -1,4 +1,12 @@
 const Express = require('express');
 const employeerouter = Express.Router();
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+employeerouter.get('/', (req, res) => {
+  db.all("SELECT * FROM Employee WHERE is_current_employee = 1", (err, rows) => {
+    res.status(200).send({employees:rows});
+  });
+});
 
 module.exports = employeerouter;
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const PORT = 4000 || process.env.PORT;
 const employeerouter = require('./api/employees.js');
 
 
-app.use('/api/employees/', employeerouter);
+app.use('/api/employees', employeerouter);
 
 app.listen(PORT, () => { console.log("Listening to Port " + PORT); });
+
+module.exports = app;
