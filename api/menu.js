c65a6a58f6429304d656c3b41d074e126d8beeaf
Leonard Struck <info@leonardstruck.de>
2018-09-03 17:31:25 +0200

    GET /api/menus

--- api/menu.js
+++ api/menu.js
@@ -3,4 +3,10 @@ const menurouter = Express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+menurouter.get('/', (req, res) => {
+  db.all('SELECT * FROM Menu', (err, rows) => {
+    res.status(200).send({menus: rows});
+  });
+});
+
 module.exports = menurouter;
